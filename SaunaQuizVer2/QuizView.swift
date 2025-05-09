8cbb5589d5a0d0c97cd986b4216dfa113a605eab
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-05-10 00:36:26 +0900

    クイズ画面の修正

--- SaunaQuizVer2/QuizView.swift
+++ SaunaQuizVer2/QuizView.swift
@@ -7,27 +7,48 @@ import SwiftUI
 
 struct QuizView: View {
     @State var isShowingScoreView = false
+    let choices = ["ライオン", "ウサイン・ボルト", "チーター", "馬"]
     
     var body: some View {
-        Text("問題番号: 1/5")
-        Text("次のうち、世界で最も速く走る動物はどれですか？")
-        
-        Button("ライオン") {
-            isShowingScoreView = true
-        }
-        .fullScreenCover(isPresented: $isShowingScoreView) {
-            ScoreView()
-        }
-        Button("ウサイン・ボルト") {
+        VStack {
+            Text("問題番号: 1/5")
+                .font(.headline)
+                .padding(10)
+                .background(.originalGreen)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
             
-        }
-        Button("チーター") {
+            Text("次のうち、世界で最も速く走る動物はどれですか？")
+                .font(.title)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.originalLightGreen)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.originalGreen, lineWidth: 5)
+                )
+                .frame(maxHeight: .infinity)
             
-        }
-        Button("牛") {
             
+            ForEach(choices, id: \.self) { choice in
+                Button() {
+                    isShowingScoreView = true
+                } label: {
+                    Text(choice)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .font(.title.bold())
+                        .background(Color.originalSkin)
+                        .foregroundStyle(.originalBrown)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .fullScreenCover(isPresented: $isShowingScoreView) {
+                            ScoreView()
+                        }
+                }
+            }
         }
-        
+        .padding()
+        .backgroundImage()
     }
 }
 
